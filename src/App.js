51ddbd7cae8206e843dd67bb7e118113a6cfaa9c
arek <alek123252@gmail.com>
2023-01-22 00:33:32 +0100

    api works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+//https://api.themoviedb.org/3/search/movie?api_key=f402a4b12e741e93d7e20be5d6f634d6&query=Jack+Reacher
+
+//https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}
+
+const API_KEY = "f402a4b12e741e93d7e20be5d6f634d6";
+const API_CODE =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDAyYTRiMTJlNzQxZTkzZDdlMjBiZTVkNmY2MzRkNiIsInN1YiI6IjYzY2M2OTU5Y2VlNDgxMDBkZWU4OWU0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6zgSiWjnA_2frOOKjORgzgAHs3tbYQfd9zGi_rhct5Y";
+
+const EXAMPLE_QUERY1 = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
+
+const EXAMPLE_QUERY2 = `
+https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Jack+Reacher`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const searchMovies = async () => {
+    const response = await fetch(`${EXAMPLE_QUERY2}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
